b19ab7a2e295efb03118ec57e5cfde0dd6fe5093
HutaJoullach <hutajoullach@gmail.com>
2024-10-07 20:26:25 +0900

    add prettierrc shadcn, fix linting errors

---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+export default {
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/lib/**/*.{js,ts,jsx,tsx}',
+    './src/index.ts',
+  ],
+}
